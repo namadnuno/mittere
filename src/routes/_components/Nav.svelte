3ebe701a42b3c9f015574c83284efe9b50588628
Nuno <n.alexandre@xgeeks.io>
2020-05-12 14:06:04 +0100

    Close meno on link click

--- src/routes/_components/Nav.svelte
+++ src/routes/_components/Nav.svelte
@@ -1,8 +1,14 @@
 <script>
-  let open = false;
+  let open = false
+  import { goto } from '@sapper/app';
+  
   const toggle = () => {
-    console.log(open, 'here')
     open = !open
+  }
+
+  const follow = (e) => {
+    goto(e.target.href);
+    toggle();
   }
 </script>
 <style>
@@ -20,22 +26,23 @@
 
   .burger .line {
     background: #484848;
-    width: 100%;
+    width: 32px;
     display: block;
     height: 4px;
     border-radius: 2px;
+    content: " ";
   }
 </style>
 
 <div class="absolute right-0 top-0">
   <button class="burger" type="button" on:click={toggle}>
-    <span class="line" />
-    <span class="line" />
-    <span class="line" />
+    <span class="line" >&nbsp; </span>
+    <span class="line" >&nbsp; </span>
+    <span class="line" >&nbsp; </span>
   </button>
 </div>
 
-<div class={open ? "bg-green-500 fixed w-full h-full z-30 flex flex-col items-center justify-center" : "invisible hidden"}>
+<div class={open ? "bg-green-500 fixed w-full h-full z-30 flex flex-col items-center justify-center top-0" : "invisible hidden"}>
     <button class="text-3xl mb-8 text-white" type="button" on:click={toggle}>
       <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1.66113 2.16116L42 42.5" stroke="#fff" stroke-width="4"/>
@@ -44,16 +51,16 @@
     </button>
     <ul class="flex flex-col items-center justify-center">
       <li class="mb-4">
-        <a href="/dashboard" class="text-3xl font-title text-white">Overview</a>
+        <a on:click|preventDefault={follow} href="/dashboard" class="text-3xl font-title text-white">Overview</a>
       </li>
       <li class="mb-4">
-        <a href="/dashboard/orders" class="text-3xl font-title text-white">My Orders</a>
+        <a on:click|preventDefault={follow} href="/dashboard/orders" class="text-3xl font-title text-white">My Orders</a>
       </li>
       <li class="mb-4">
-        <a href="/dashboard/orders/create" class="text-3xl font-title text-white">Create Order</a>
+        <a on:click|preventDefault={follow} href="/dashboard/orders/create" class="text-3xl font-title text-white">Create Order</a>
       </li>
       <li class="mb-4">
-        <a href="/dashboard/orders/preferences" class="text-3xl font-title text-white">Preferences</a>
+        <a on:click|preventDefault={follow} href="/dashboard/orders/preferences" class="text-3xl font-title text-white">Preferences</a>
       </li>
       <li class="mb-4">
         <button type="button" class="text-3xl font-title text-white">Logout</button>
